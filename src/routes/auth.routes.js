bab946e012cfd35b71007bdd019ee8a8d23d67fa
Siddique Abdul <szuber558@gmail.com>
2026-07-14 16:03:10 +0530

    Backend

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,3 +1,39 @@
 const { Router } = require("express");
-
+const authController = require("../controllers/auth.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 const authRouter = Router();
+
+/**
+ * @route POST /api/auth/register
+ * @desc register a new user
+ * @access Public
+ */
+authRouter.post("/register", authController.registerUserController);
+
+/**
+ * @route POST /api/auth/login
+ * @desc login with existing user
+ * @access Public
+ */
+authRouter.post("/login", authController.loginUserController);
+
+/**
+ * @route GET /api/auth/logout
+ * @desc logout the existing user (clear token from user cookie and add token in the blacklist)
+ * @access Public
+ */
+authRouter.get("/logout", authController.logoutUserController);
+
+/**
+ * @route GET /api/auth/get-me
+ * @desc get the current logged in user details
+ * @access Private
+ */
+
+authRouter.get(
+  "/get-me",
+  authMiddleware.authUser,
+  authController.getMeController,
+);
+
+module.exports = authRouter;
