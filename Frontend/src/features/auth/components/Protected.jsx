dee0e47238dd01299229a980c706cab8fc64bd8d
Siddique Abdul <szuber558@gmail.com>
2026-07-25 00:32:44 +0530

    Interview ai done

--- Frontend/src/features/auth/components/Protected.jsx
+++ Frontend/src/features/auth/components/Protected.jsx
@@ -1,20 +1,20 @@
 import { useAuth } from "../hooks/useAuth";
 import { Navigate } from "react-router";
-const Protected = ({ children }) => {
-  const { loading, user } = useAuth();
-  if (loading) {
-    return (
-      <main>
-        <h1>Loading...</h1>
-      </main>
-    );
-  }
+import React from 'react'
 
-  if (!user) {
-    return <Navigate to={"/login"} />;
-  }
+const Protected = ({children}) => {
+    const { loading,user } = useAuth()
 
-  return children;
-};
 
-export default Protected;
+    if(loading){
+        return (<main><h1>Loading...</h1></main>)
+    }
+
+    if(!user){
+        return <Navigate to={'/login'} />
+    }
+    
+    return children
+}
+
+export default Protected
